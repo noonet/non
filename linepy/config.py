999b10a139d7690366ad9ac94b8ba53014203c6a
RSTjeser <50651103+RSTjeser@users.noreply.github.com>
2019-09-16 20:30:13 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -29,8 +29,24 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[400]
-    APP_VER     = '5.5.5'
+    APP_VERSION = {
+        'ANDROID': '8.14.2',
+        'IOS': '8.14.2',
+        'ANDROIDLITE': '2.1.0',
+        'BIZANDROID': '1.7.2',
+        'BIZIOS': '1.7.5',
+        'BIZWEB': '1.0.22',
+        'DESKTOPWIN': '5.9.0',
+        'DESKTOPMAC': '5.9.0',
+        'IOSIPAD': '8.14.2',
+        'CHROMEOS': '2.1.5',
+        'WIN10': '5.5.5',
+        'DEFAULT': '8.11.0',
+        'CLOVAFRIENDS':'5.5.1'
+    }
+
+    APP_TYPE    = 'IOSIPAD'
+    APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'Fc.Pea150'
     SYSTEM_VER  = '11.2.5'
